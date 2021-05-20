dcbfedfca94cfadbac8f817f4bbd78dbfae8d868
wyc <wwwwyccom@163.com>
2021-05-20 22:37:28 +0800

    add gitignore

---
+++ src/main.asm
@@ -0,0 +1,28 @@
+.386
+.model flat,stdcall
+option casemap:none
+
+includelib msvcrt.lib
+includelib acllib.lib
+
+
+include inc\acllib.inc
+include inc\sharedVar.inc
+
+printf proto c:dword,:vararg
+
+.data
+hellomsg sbyte "gogogo!%d,%d"
+
+
+.code
+main proc;梦开始的地方
+;	invoke printf,offset hellomsg
+	mov psin[0],0FFFFh
+	mov psin[4],03h;提示：数组下标表示字节偏移量而非元素偏移量
+	invoke printf,offset hellomsg,psin[4],psin[0]
+
+	ret
+main endp
+
+end main
---
+++ src/sharedVar.asm
@@ -0,0 +1,26 @@
+.386
+.model flat,stdcall
+option casemap:none
+
+
+
+.data
+currWindow dd 0
+public currWindow
+
+cdeg dd 0
+public cdeg
+
+pindeg dd 360 DUP(0)
+public pindeg
+
+pinnum dd 0
+public pinnum
+
+psin dd 360 DUP(0)
+public psin
+
+pcos dd 360 DUP(0)
+public pcos
+
+end
