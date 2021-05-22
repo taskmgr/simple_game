f49b46edd2167419b55c5acb6d24b7a4dbe8fa93
I-Rinka <I_Rinka@outlook.com>
2021-05-22 09:59:43 +0800

    well, it shows window now

--- src/main.asm
+++ src/main.asm
@@ -13,14 +13,17 @@ printf proto c:dword,:vararg
 
 .data
 hellomsg sbyte "gogogo!%d,%d"
-
+hello sbyte	"hello"
 
 .code
 main proc;梦开始的地方
 ;	invoke printf,offset hellomsg
-	mov psin[0],0FFFFh
-	mov psin[4],03h;提示：数组下标表示字节偏移量而非元素偏移量
-	invoke printf,offset hellomsg,psin[4],psin[0]
+	;mov psin[0],0FFFFh
+	;mov psin[4],03h;提示：数组下标表示字节偏移量而非元素偏移量
+	;invoke printf,offset hellomsg,psin[4],psin[0]
+	invoke	init_first
+	invoke	initWindow,offset	 hello,DEFAULT,DEFAULT,500,500
+	invoke	init_second
 
 	ret
 main endp
