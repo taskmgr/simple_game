b70d0cb8290f7b0224703f7384b7fb64dc3f3f2c
I-Rinka <I_Rinka@outlook.com>
2021-05-22 13:53:33 +0800

    Simple control runs

--- src/main.asm
+++ src/main.asm
@@ -8,24 +8,40 @@ includelib acllib.lib
 
 include inc\acllib.inc
 include inc\sharedVar.inc
+include inc\controller.inc
 
 printf proto c:dword,:vararg
+mouseEvent proto C:DWORD,:DWORD,:DWORD,:DWORD
 
 .data
-hellomsg sbyte "gogogo!%d,%d"
-hello sbyte	"hello"
+hello sbyte	"hello",0
+coord sbyte "%d,%d",10,0
+
 
 .code
+
+iface_mouseEvent proc C x:dword,y:dword,button:dword,event:dword
+	mov ecx,event
+	cmp ecx,BUTTON_DOWN
+	jne not_click
+
+		invoke printf,offset coord,x,y
+
+	not_click:
+	ret 
+iface_mouseEvent endp
+
 main proc;梦开始的地方
-;	invoke printf,offset hellomsg
-	;mov psin[0],0FFFFh
-	;mov psin[4],03h;提示：数组下标表示字节偏移量而非元素偏移量
-	;invoke printf,offset hellomsg,psin[4],psin[0]
+
 	invoke	init_first
-	invoke	initWindow,offset	 hello,DEFAULT,DEFAULT,500,500
+	invoke	initWindow,offset hello,DEFAULT,DEFAULT,500,500
+	push	iface_mouseEvent
+	call registerMouseEvent
+	add esp,4
 	invoke	init_second
 
 	ret
 main endp
+
 
 end main
