c8be38b9c4b5bb15d787731e7fb714d77dd1f92d
I-Rinka <I_Rinka@outlook.com>
2021-05-22 14:21:44 +0800

    Modulerize

--- src/controller.asm
+++ src/controller.asm
@@ -1,8 +1,26 @@
 .386
 .model flat,stdcall
 option casemap:none
+includelib msvcrt.lib
+includelib acllib.lib
+include inc\acllib.inc
 
+printf proto C :dword,:vararg
 
+.data
+coord sbyte "%d,%d",10,0
+
+.code
+	iface_mouseEvent proc C x:dword,y:dword,button:dword,event:dword
+		mov ecx,event
+		cmp ecx,BUTTON_DOWN
+		jne not_click
+
+			invoke printf,offset coord,x,y
+
+		not_click:
+		ret 
+	iface_mouseEvent endp
 
 
 end
--- src/main.asm
+++ src/main.asm
@@ -11,25 +11,11 @@ include inc\sharedVar.inc
 include inc\controller.inc
 
 printf proto c:dword,:vararg
-mouseEvent proto C:DWORD,:DWORD,:DWORD,:DWORD
 
 .data
 hello sbyte	"hello",0
-coord sbyte "%d,%d",10,0
-
 
 .code
-
-iface_mouseEvent proc C x:dword,y:dword,button:dword,event:dword
-	mov ecx,event
-	cmp ecx,BUTTON_DOWN
-	jne not_click
-
-		invoke printf,offset coord,x,y
-
-	not_click:
-	ret 
-iface_mouseEvent endp
 
 main proc;梦开始的地方
 
