883f907c858b818869c531d3d44f5e45ae49ac45
I-Rinka <I_Rinka@outlook.com>
2021-05-22 14:26:03 +0800

    before merge

--- src/main.asm
+++ src/main.asm
@@ -8,7 +8,7 @@ includelib acllib.lib
 
 include inc\acllib.inc
 include inc\sharedVar.inc
-include inc\controller.inc
+include inc\controller.inc ;CONTROLLER
 
 printf proto c:dword,:vararg
 
@@ -21,9 +21,9 @@ main proc;梦开始的地方
 
 	invoke	init_first
 	invoke	initWindow,offset hello,DEFAULT,DEFAULT,500,500
-	push	iface_mouseEvent
-	call registerMouseEvent
-	add esp,4
+
+	invoke registerMouseEvent,iface_mouseEvent ;和C语言一样的调用方式
+
 	invoke	init_second
 
 	ret
