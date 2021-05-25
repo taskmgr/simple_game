2e2805dcbd8fa689b28fed9807734e28a77fc398
Llllrf <1269957412@qq.com>
2021-05-25 09:32:15 +0800

    update UI

--- src/main.asm
+++ src/main.asm
@@ -19,15 +19,15 @@ main proc;梦开始的地方
 	invoke initWindow, offset winTitle, 425, 50, 550, 700 
 	invoke loadMenu, 0  ;显示主菜单
 	
-	invoke loadMenu, 3
-	mov pindeg[0], 0
-	mov pindeg[4], 180
-	mov pindeg[8], 90
-	mov pinnum, 12
-	invoke FlushScore, 20
-	invoke initGameWindow, 3, 10
-	mov pindeg[12], 45
-	mov pinnum, 11
+	;invoke loadMenu, 3
+	;mov pindeg[0], 0
+	;mov pindeg[4], 180
+	;mov pindeg[8], 90
+	;mov pinnum, 12
+	;invoke FlushScore, 20
+	;invoke initGameWindow, 3, 10
+	;mov pindeg[12], 45
+	;mov pinnum, 11
 	invoke init_second
 	ret
 main endp
